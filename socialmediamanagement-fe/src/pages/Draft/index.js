12b4a6677148ff22e69112d6edc1e91e6c972d26
fedorarwp <fedorar2000@gmail.com>
2022-08-30 19:34:44 +0700

    register page and modal addition

--- socialmediamanagement-fe/src/pages/Draft/index.js
+++ socialmediamanagement-fe/src/pages/Draft/index.js
@@ -1,23 +1,82 @@
-import React from 'react';
-import style from './style.module.css';
+import { Dropdown, Menu, Modal } from "antd";
+import React from "react";
+import style from "./draft.module.css";
+import { DeleteOutlined } from "@ant-design/icons";
+import { useState } from "react";
 
 function Draft() {
-    return (
+  const [open, setOpen] = useState(false);
+  const [currentData, setCurrentData] = useState({});
+  const handleOpen = (param) => {
+    setCurrentData(param);
+    setOpen(true);
+  };
+  const tweets = [
+    {
+      id: 1,
+      title: "Tweet ini ditulis oleh Ghazel si PM Beta",
+      date: "2022-08-26",
+    },
+    {
+      id: 2,
+      title: "Tweet ini ditulis oleh Fefe si FE Beta",
+      date: "2022-08-27",
+    },
+    {
+      id: 3,
+      title: "Tweet ini ditulis oleh Ez si BE Beta",
+      date: "2022-08-28",
+    },
+  ];
+
+  return (
     <div className={style.wrapper}>
-        <div className={style.postItem}>
+      {tweets.map((tweet) => (
+        <div key={tweet.id} className={style.postItem}>
           <div className={style.card}>
-            <p>This is an example of a drafted tweet that has not been posted yet, so the user has two options: to post now or schedule it for later</p>
-            <button className={style.btnMore}>...</button>
+            {tweet.title}
+            <Dropdown
+              placement="bottomRight"
+              overlay={
+                <Menu
+                  items={[
+                    {
+                      key: "delete",
+                      label: (
+                        <span>
+                          <DeleteOutlined />
+                          &ensp; Delete Post
+                        </span>
+                      ),
+                    },
+                  ]}
+                />
+              }
+            >
+              <button className={style.btnMore}>...</button>
+            </Dropdown>
           </div>
           <div className={style.btnContainer}>
-          <button className={style.btn}>Post Now</button>
+            <button className={style.btn} onClick={() => handleOpen(tweet)}>
+              Post Now
+            </button>
           </div>
           <div className={style.btnContainer}>
-          <button className={style.btn}>Schedule</button>
+            <button className={style.btn}>Schedule</button>
           </div>
         </div>
-      </div>
-    );
-  }
+      ))}
+
+      <Modal
+        title="Are you sure you want to post now?"
+        visible={open}
+        onOk={() => setOpen(false)}
+        onCancel={() => setOpen(false)}
+      >
+        <h3>{currentData.title}</h3>
+      </Modal>
+    </div>
+  );
+}
 
 export default Draft;
