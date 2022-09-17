a2babddc1b5c2b2808f566d3384813d189d7880e
fedorarwp <fedorar2000@gmail.com>
2022-09-17 10:06:49 +0700

    sign up backend integration

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,34 +1,78 @@
 import React from "react";
 import style from "./register.module.css";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, notification } from "antd";
 import "antd/dist/antd.css";
 import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
 
 function Register() {
   const navigate = useNavigate();
+  // const handleNewAccount = (values) => {
+  //   const { username, email, password } = values;
+  //   axios
+  //     .post("http://40.71.112.112/sign_up", {
+  //       username,
+  //       email,
+  //       password,
+  //     })
+  //     .then((res) => {
+  //       console.log(res);
+  //       notification.success({
+  //         message: res.data.Message,
+  //       });
+  //       navigate("/login");
+  //     })
+  //     .catch((err) => {
+  //       notification.error({
+  //         message: err.message,
+  //       });
+  //     });
+  // };
+
+  const createNewAccount = async (values) => {
+    try {
+      const { username, email, password } = values;
+      const res = await axios.post("http://40.71.112.112/sign_up", {
+        username,
+        email,
+        password,
+      });
+      notification.success({
+        message: res.data.Message,
+      });
+      navigate("/login");
+    } catch (error) {
+      notification.error({
+        message: error.message,
+      });
+    }
+  };
+
   return (
     <div className={style.wrapper}>
       <div className={style.leftWrapper}>
         <div className={style.formContainer}>
-        <h2><b>Start creating your own account</b></h2>
-          <Form labelCol={{ span: 8 }} onFinish={() => navigate("/workspaces")}>
-
-          <div className={style.fullNameContainer}>
-              <label>Full Name</label>
+          <h2>
+            <b>Start creating your own account</b>
+          </h2>
+          <Form
+            labelCol={{ span: 8 }}
+            // onFinish={() => navigate("/workspaces")}
+            onFinish={(values) => createNewAccount(values)}
+          >
+            <div className={style.fullNameContainer}>
+              <label>Username</label>
               <Form.Item
-                name="fullName"
+                name="username"
                 rules={[
                   {
                     required: true,
-                    message: "Please enter your full name",
+                    message: "Please enter your username",
                   },
                 ]}
                 hasFeedback
               >
-                <Input
-                  size="large"
-                  placeholder="Insert your full name"
-                />
+                <Input size="large" placeholder="Insert your username" />
               </Form.Item>
             </div>
 
@@ -45,10 +89,7 @@ function Register() {
                 ]}
                 hasFeedback
               >
-                <Input
-                  size="large"
-                  placeholder="Insert your email"
-                />
+                <Input size="large" placeholder="Insert your email" />
               </Form.Item>
             </div>
 
@@ -76,13 +117,18 @@ function Register() {
                 htmlType="submit"
                 type="primary"
                 className={style.btn}
-                style={{ background: "black", border:"black" }}
+                style={{ background: "black", border: "black" }}
               >
                 Register
               </Button>
             </div>
           </Form>
-          Already have an account? <b><Link style={{color: 'black'}} to='/login'>Log in</Link></b>
+          Already have an account?{" "}
+          <b>
+            <Link style={{ color: "black" }} to="/login">
+              Log in
+            </Link>
+          </b>
         </div>
       </div>
 
